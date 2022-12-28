8cad907ee1fed4ab229e50cceda79249373b171b
Kowshika K <kowshi0101@gmail.com>
2022-12-28 23:27:41 +0530

    this keyword

--- newpr.js
+++ newpr.js
@@ -1,38 +1,43 @@
-//call apply bind
-
-
-/*let obj={
-    fname : 'ABC',
-    lname : 'DEF'
+//Global scope, class, object, method, function, prototype.
+//Global scope
+/*this.b='sunflower'
+console.log(b);
+console.log(window.b)
+this.a={
+    flower : 'rose'
+};
+console.log(this.a.flower);*/
+//object and method
+/*let a={
+    flower : 'lotus',
+    b(){
+        console.log(`Flower = ${this.flower}`);
+    }
+};
+a.b();*/
+//function
+/*this.flower='sunflower'
+const a=function(flower2){
+    console.log(`Flower = ${this.flower} flower2 = ${flower2}`);
 }
-let func=function(met){
-    console.log(`${met} - First Name = ${this.fname} and Last Name = ${this.lname}`);
+a.call(this,'lotus')*/
+//prototype
+/*const a=function(flower){
+    this.flower1 = flower
+};
+a.prototype.b=function(flower2){
+    console.log(`Flower = ${this.flower1} , flower2 = ${flower2}`);
 }
-func.call(obj, 'Call');
-func.apply(obj, ['Apply']);
-let bi=func.bind(obj, 'Bind');
-bi();*/
-
-/*let Student={
-    age : 20
-}
-let func = function(){
-    console.log(`Age = ${this.age}`);
-}
-let bi=func.bind(Student);
-bi();*/
-
-// currying
-/*let Student= function(name,result){
-    console.log(name,result)
-}
-let cur=Student.bind(this, 'ABC');
-cur('P')*/
-
-/*let Student1= function(name1){
-    return function(result1){
-        console.log(name1,result1);
+const flowers=new a('Lotus');
+flowers.b('rose')*/
+//class
+/*class Flower{
+    constructor(flower){
+        this.flower1 = flower
+    }
+    b(flower2){
+        console.log(`Flower1 = ${this.flower1} , flower2 = ${flower2}`);
     }
 }
-let cur=Student1('ABC');
-cur('P')*/
+const flowers=new Flower('Lotus');
+flowers.b('rose')*/
