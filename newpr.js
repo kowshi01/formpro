998cdcb702811601cb15c7e4944cdddee88a15a7
Kowshika K <kowshi0101@gmail.com>
2022-12-28 19:58:14 +0530

    call apply bind

--- newpr.js
+++ newpr.js
@@ -1,10 +1,37 @@
+//call apply bind
 
-    /*let myobj={
-        name1 : 'abc',
-        emailid : 'abc@gmail.com'
-     }
-  
-  let myobj_ser=JSON.stringify(myobj);
-  localStorage.setItem("MyObj",myobj_ser);
-  let myobj_deser=JSON.parse(localStorage.getItem("MyObj"));
-  console.log(myobj_deser);*/
+/*let obj={
+    fname : 'ABC',
+    lname : 'DEF'
+}
+let func=function(met){
+    console.log(`${met} - First Name = ${this.fname} and Last Name = ${this.lname}`);
+}
+func.call(obj, 'Call');
+func.apply(obj, ['Apply']);
+let bi=func.bind(obj, 'Bind');
+bi();*/
+
+/*let Student={
+    age : 20
+}
+let func = function(){
+    console.log(`Age = ${this.age}`);
+}
+let bi=func.bind(Student);
+bi();*/
+
+// currying
+/*let Student= function(name,result){
+    console.log(name,result)
+}
+let cur=Student.bind(this, 'ABC');
+cur('P')*/
+
+/*let Student1= function(name1){
+    return function(result1){
+        console.log(name1,result1);
+    }
+}
+let cur=Student1('ABC');
+cur('P')*/
