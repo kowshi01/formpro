8397f31484f6df73da4ff459e341656202411478
Kowshika K <kowshi0101@gmail.com>
2023-01-06 11:24:36 +0530

    promises

--- newpr.js
+++ newpr.js
@@ -1,40 +1,71 @@
-const Myform=document.getElementById('form');
-Myform.addEventListener('submit',onsubmit)
-
-function onsubmit(e){
-    e.preventDefault();
-    const amount=document.getElementById('Eamount').value;
-    const des=document.getElementById('des').value;
-    const cat=document.getElementById('category').value;
-    let obj={
-        Eamount : amount,
-        Edes : des,
-        Ecat : cat
+var b=0;
+const post=[
+    {title : 'Post1'},
+    {title : 'Post2'},
+    {title : 'Post3'}
+];
+function getPost(){
+    setTimeout(() => {
+    let output='';
+    for(let i=0;i<post.length;i++){
+        output+=`<li>${post[i].title}</li>`;
+        updateLastUserActivityTime(new Date().getTime());
     }
-   localStorage.setItem(obj.Edes,JSON.stringify(obj));   
-   var objde=JSON.parse(localStorage.getItem(obj.Edes));
-   newuser(objde);
-  }
-  function newuser(nuser){
-       let parentNode=document.getElementById('output');
-       let childHTML=`<h4 id='heading'> ${nuser.Eamount} - ${nuser.Edes} - ${nuser.Ecat}<button onclick=edituser('${nuser.Eamount},${nuser.Edes},${nuser.Ecat}')>Edit</button>
-           <button onclick=deluser('${nuser.Edes}')>Delete</button></h4>`;
-       parentNode.innerHTML=parentNode.innerHTML+childHTML;
-    }
-    function edituser(amount,des,cat){
-      amount=document.getElementById('Eamount').value;
-      des=document.getElementById('des').value;
-      cat=document.getElementById('category').value;
-      deluser(des);
-   }  
-   function deluser(des) {
-      localStorage.removeItem(des);
-      removeFromScreen(des);
-   }  
-  function removeFromScreen(des){
-      const parentNodee=document.getElementById('output');
-      const childNodee=document.getElementById('heading');
-      if(childNodee){
-      parentNodee.removeChild(childNodee); 
-      }
-   }
+    
+    document.body.innerHTML=output;
+    }, 1000);
+}
+function create4thPost(posts){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+            post.push(posts);
+            //updateLastUserActivityTime(new Date().getTime());
+            const error=false;
+            if(!error){
+                resolve();
+            }else{
+                reject("error");
+            }
+        }, 1000);
+    });
+}
+function updateLastUserActivityTime(a){
+    new Promise((resolve)=>{
+        setTimeout(() => {
+            if(a>0){
+                b=a;
+                resolve();
+            }
+            console.log(b);
+        }, 1000);
+});
+}
+function delPost(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            if(post.length>0){
+                resolve(post.pop());
+            }else{
+                reject("Error : Array is empty now");
+            }
+        },1000);
+    });
+}
+create4thPost({title :'post4'})
+.then(()=>{
+    getPost()
+    delPost().then(()=>{
+        getPost()
+        delPost().then(()=>{
+            getPost()
+            delPost().then(()=>{
+                getPost()
+                updateLastUserActivityTime()
+                delPost().then(()=>{
+                    getPost()
+                    delPost().catch(err=>console.log(err));
+                })
+            })
+        })
+    })
+})
